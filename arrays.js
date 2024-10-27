a43ccab86ab43ccbe4f8e3ebd77844cc6f4948fd
Nancy20m4c <101662250+Nancy20m4c@users.noreply.github.com>
2024-10-27 12:18:50 +0100

    array names modify

--- arrays.js
+++ arrays.js
@@ -25,3 +25,20 @@ letters.shift();
 letters.pop();
 
 console.log(letters);
+
+// arrays Names
+let data = [
+  {name: "Nacho", telephone: "966112233", age: 40},
+  {name: "Ana", telephone: "911223344", age: 35},
+  {name: "Mario", phone: "611998877", age: 15},
+  {name: "Laura", telephone: "633663366", age: 17}
+  ];
+
+  data.push({name: "Pedro", telephone: "611944444", age: 25});
+  data.push({name: "Julia", phone: "633232323", age: 37});
+
+  console.log(data);
+
+  data.sort((a, b) => a.age - b.age);
+
+  console.log(data);
