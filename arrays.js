6e011ba35cf77230918b0f75872d1bdfee0a4ab8
Nancy20m4c <101662250+Nancy20m4c@users.noreply.github.com>
2024-10-27 13:26:01 +0100

    funtion con math.random

--- arrays.js
+++ arrays.js
@@ -109,3 +109,17 @@ for (let i = 0; i < 50; i++) {
   const combinacion = generarCombinacionPrimitiva();
   console.log(combinacion);
 }
+
+// math.random 
+
+const frecuencias = new Array(10).fill(0);
+
+for (let i = 0; i < 10000; i++) {
+  const numeroAleatorio = Math.floor(Math.random() * 10) + 1;
+
+  frecuencias[numeroAleatorio - 1]++;
+}
+console.log("Frecuencias:");
+for (let i = 0; i < frecuencias.length; i++) {
+  console.log(`Número ${i + 1}: ${frecuencias[i]}`);
+}
